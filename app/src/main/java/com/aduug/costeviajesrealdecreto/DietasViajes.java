b57224a2150c2589770276a51f1d4d0dd4589d31
ZeteESP <zete0@hotmail.com>
2016-02-29 20:50:24 +0100

    Incluye grupos profesionales y inicio Spinner

---
+++ app/src/main/java/com/aduug/costeviajesrealdecreto/DietasViajes.java
@@ -0,0 +1,54 @@
+package com.aduug.costeviajesrealdecreto;
+
+
+public class DietasViajes {
+
+    private double aloj;
+    private double manu;
+    private int dias;
+    private int pers;
+
+    public DietasViajes (double aloj,double manu,int dias, int pers){
+        this.aloj=aloj;
+        this.manu=manu;
+        this.dias=dias;
+        this.pers=pers;
+
+    }
+    public double calculaCoste(){
+
+        double costeTotal = 0;
+
+//        this.aloj=aloj*pers;
+//        this.manu=manu*pers;
+
+        costeTotal=calculaCosteAloj()+ calculaCosteManu();
+
+        return costeTotal;
+
+    }
+
+    public double calculaCosteAloj(){
+
+        if (dias==0){
+            return 0;
+        }else{
+            double costeAloj = 0;
+
+            costeAloj=(aloj*pers)*dias;
+
+            return costeAloj;
+        }
+    }
+
+    public double calculaCosteManu(){
+
+        double costeManu = 0;
+
+        costeManu=(manu*pers)*(dias+1);
+
+        return costeManu;
+
+    }
+
+}
--- app/src/main/java/com/aduug/costeviajesrealdecreto/MainActivity.java
+++ app/src/main/java/com/aduug/costeviajesrealdecreto/MainActivity.java
@@ -8,10 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     String titulo;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        List list = new ArrayList();
+        //list.toArray(new String[] { "España", "Reino Unido", "Alemania", "Francia", "Italia", "Estados Unidos", "Suecia", "Suiza", "Austria", "Noruega", "Países Bajos", "Polonia", "Portugal", "Canadá", "Finlandia", "China", "Japón", "India", "Irlanda", "Dinamarca", "Bélgica", "Luxemburgo", "Mexico", "Andorra", "Angola", "Arabia Saudita", "Argelia", "Argentina", "Australia", "Bolivia", "Bosnia-Herzegovina", "Brasil", "Bulgaria", "Camerún", "Chile", "Colombia", "Corea", "Costa de Marfil", "Costa Rica", "Croacia", "Cuba", "R.Dominicana", "Ecuador", "Egipto", "El Salvador", "Emiratos Árabes Unidos", "Eslovaquia", "Etiopía", "Filipinas", "Gabón", "Ghana", "Grecia", "Guatemala", "Guinea Ecuatorial", "Haití", "Honduras", "Hong Kong", "Hungría", "Indonesia", "Irak", "Iran", "Israel", "Italia", "Jamaica", "Jordania", "Kenia", "Kuwait", "Líbano", "Libia", "Malasia", "Malta", "Marruecos", "Mauritania", "Mozambique", "Nicaragua", "Nigeria", "Nueva Zelanda", "Pakistán", "Panamá", "Paraguay", "Perú", "República Checa", "Rumania", "Rusia", "Senegal", "Singapur", "Siria", "Sudáfrica", "Tailandia", "Taiwán", "Tanzania", "Túnez", "Turquía", "Uruguay", "Venezuela", "Yemen", "Yugoslavia", "Zaire/Congo", "Zimbawe", "Resto del Mundo" });
+        list.add("España");
+        list.add("Francia");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,list);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
 
 
         //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
